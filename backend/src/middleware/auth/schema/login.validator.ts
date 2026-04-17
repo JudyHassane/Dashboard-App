b4a820b187b21afb0cd46ed92047989af07af535
JudyHassane <joudyhassane@hotmail.com>
2026-04-17 18:29:22 +0300

    feat: Add auth schema validators

---
+++ backend/src/middleware/auth/schema/login.validator.ts
@@ -0,0 +1,20 @@
+import Joi from "joi";
+import { validator } from "../../validator";
+
+// LOGIN schema
+const loginSchema = Joi.object({
+  email: Joi.string().trim().email().lowercase().max(254).required().messages({
+    "any.required": "Email is required",
+    "string.empty": "Email is required",
+    "string.email": "Invalid email format",
+    "string.max": "Email must be at most 254 characters",
+  }),
+  password: Joi.string().trim().min(8).max(64).required().messages({
+    "any.required": "Password is required",
+    "string.empty": "Password is required",
+    "string.min": "Password must be at least 8 characters",
+    "string.max": "Password must be at most 64 characters",
+  }),
+});
+
+export const validateLogin = validator(loginSchema);
---
+++ backend/src/middleware/auth/schema/register.validator.ts
@@ -0,0 +1,35 @@
+import Joi from "joi";
+import { validator } from "../../validator";
+
+// REGISTER schema
+const registerSchema = Joi.object({
+  name: Joi.string().trim().min(3).max(50).required().messages({
+    "any.required": "Name is required",
+    "string.empty": "Name is required",
+    "string.min": "Name must be at least 3 characters",
+    "string.max": "Name must be at most 50 characters",
+  }),
+  email: Joi.string().trim().email().lowercase().max(254).required().messages({
+    "any.required": "Email is required",
+    "string.empty": "Email is required",
+    "string.email": "Invalid email format",
+    "string.max": "Email must be at most 254 characters",
+  }),
+  password: Joi.string().trim().min(8).max(64).required().messages({
+    "any.required": "Password is required",
+    "string.empty": "Password is required",
+    "string.min": "Password must be at least 8 characters",
+    "string.max": "Password must be at most 64 characters",
+  }),
+  confirmPassword: Joi.string()
+    .trim()
+    .valid(Joi.ref("password"))
+    .required()
+    .messages({
+      "any.required": "Please confirm your password",
+      "string.empty": "Please confirm your password",
+      "any.only": "Passwords do not match",
+    }),
+});
+
+export const validateRegister = validator(registerSchema);
