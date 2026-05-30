2183e4ba7cb9a4f021c32db9ff3fabe70a553b75
JudyHassane <joudyhassane@hotmail.com>
2026-05-30 18:04:36 +0300

    feat: Add book, author and category entities

---
+++ backend/src/orm/entities/books/enums.ts
@@ -0,0 +1,4 @@
+export enum BookStatus {
+  AVAILABLE = "available",
+  OUT_OF_STOCK = "out of stock",
+}
---
+++ backend/src/orm/entities/categories/category.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Book } from "../books/book.entity";
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "varchar", unique: true })
+  name!: string;
+
+  @OneToMany(() => Book, (book) => book.category) books!: Book[];
+}
